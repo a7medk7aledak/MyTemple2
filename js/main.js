a8e458374f76f6fab9f3895158fb7461227573ad
a7medk7aledak <92623979+a7medk7aledak@users.noreply.github.com>
2023-07-16 19:50:20 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// function changeBg() {
+//     let myEle = document.querySelector(".bake-header1")
+//     myEle.style.backgroundImage = `url(../images/header1.jpg)`;
+// }
+// setInterval(changeBg,1000)
+// function changeBg2() {
+//     let myEle = document.querySelector(".bake-header1")
+//     myEle.style.backgroundImage = `url(../images/header.jpg)`;
+// }
+// setInterval(changeBg2,2000)
