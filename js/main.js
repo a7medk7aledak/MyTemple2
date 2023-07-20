5c373e6fcb69c37112d27f89706802018cea2953
a7medk7aledak <92623979+a7medk7aledak@users.noreply.github.com>
2023-07-20 19:20:45 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,36 @@
-// function changeBg() {
-//     let myEle = document.querySelector(".bake-header1")
-//     myEle.style.backgroundImage = `url(../images/header1.jpg)`;
-// }
-// setInterval(changeBg,1000)
-// function changeBg2() {
-//     let myEle = document.querySelector(".bake-header1")
-//     myEle.style.backgroundImage = `url(../images/header.jpg)`;
-// }
-// setInterval(changeBg2,2000)
+// mask2
+(function height() {
+    let img = document.querySelector(".mask2 .container .graph img");
+    let mask2 = document.querySelector(".mask2");
+    window.onresize = function() {
+        mask2.style.height = `${img.height}px`
+    }
+    window.onload = function() {
+        mask2.style.height = `${img.height}px`
+    }
+})();
+
+
+// event click and nav
+let buttom = document.querySelector("header .nav-bar .menu");
+let nav = document.getElementsByTagName("nav")[0]
+let icon = document.querySelector("nav .head i")
+let navBar = document.querySelector("header .nav-bar .menu")
+
+buttom.onclick = function () {
+    if (nav.classList[0]==="main-nav") {
+        nav.classList.remove("main-nav");
+        nav.style.right = "0"
+        navBar.style.cssText = "position: none;";
+    }
+    else {
+        nav.classList.add("main-nav");
+        nav.style.right = "0"
+        navBar.style.cssText = "position: absolute;right: 300px;";
+    }
+}
+icon.onclick = function () {
+    nav.classList.remove("main-nav");
+    nav.style.right = "0"
+    navBar.style.cssText = "position: none;";
+}
